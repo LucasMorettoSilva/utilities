4e62bf0cee2a2a20430acc06c5c022bea566fb91
Lucas Moretto <lucasmoretto16@gmail.com>
2022-04-06 15:31:42 -0300

    Update mount_minix.sh

--- mount_minix.sh
+++ mount_minix.sh
@@ -9,14 +9,14 @@
 # e desligue-a através do comando shutdown no Virtual Box para
 # evitar corrupção de arquivos. 
 
-# Faça cópias/backups da imagem (.ova) e use git. Caso você inicie 
+# Faça cópias/backups da VDI e use git. Caso você inicie 
 # a VM sem antes desmontar a partição, a imagem será corrompida.
 
 # Como usar: 
-#     - Defina o local onde o script vai montar a partição da imagem (.ova)
+#     - Defina o local onde o script vai montar a partição a partir da VDI (.vdi)
 #     - Execute "mount_minix.sh modprobe" para ativar o módulo do kernel com as configurações corretas
-#     - Use "mount_minix.sh m" para montar (transformar a imagem .ova numa partição da sua máquina)
-#     - Use "mount_minix.sh u" para desmontar (converter a partição para imagem .ova)
+#     - Use "mount_minix.sh m" para montar (transformar a VDI numa partição da sua máquina)
+#     - Use "mount_minix.sh u" para desmontar (converter a partição para .vdi)
 
 minix_vdi="/home/lucas/VirtualBox VMs/Minix3/Minix3.vdi"
 mountpoint="/mnt/minix"
